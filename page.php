9909f98569263838a5fd44b3d082e6a811c2e6d7
Werbeagenten <werbeagenten@WerbeagntensMBP.fritz.box>
2019-10-22 09:28:57 +0200

    pfage style changes

--- page.php
+++ page.php
@@ -17,7 +17,7 @@
 
 
 
- <div class="row align-middle align-center">
+ <div class="row align-middle align-center" style="margin-top:40px">
    <div class="columns small-12 medium-6 main-content">
      <?php the_title('<h1 class="text-center">', '</h1>'); ?>
      <?php the_content(); ?>
